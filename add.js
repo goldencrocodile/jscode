e04f808bcd1ecb54f409ea1fdd4005643ef05a44
Madhu <madhumandanna@gmail.com>
2018-08-01 17:10:31 +0530

    add function

---
+++ add.js
@@ -0,0 +1,16 @@
+//function add
+//add takes 2 arguments
+//n1 =10 , n2 =20 add function should return 30
+//n1 ='hello' , n2 = 'world', add function should return 'hello world'
+
+function add(n1,n2){
+
+    return n1 + n2 ;
+}
+
+var n1 = 10;
+var n2 = 20;
+var result = add(n1,n2);
+console.log(result);
+
+console.log(add('hello','World'));
